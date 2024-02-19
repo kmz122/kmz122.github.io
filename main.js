4f6efd28b1badb8d0aea64a37ca390f27d6d8201
kmz122 <kmzgithub122@gmail.com>
2024-02-19 23:29:29 +0630

    update: profile details

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function () {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    });
+  // Get the current year
+  var currentYear = new Date().getFullYear()
 
-    $('.top-nav .nav-link').on('click', function () {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
+  // Update the content of the span element with the current year
+  $('#currentYear').text(currentYear)
 
-    $('nav a[href*="#"]').on('click', function () {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, 2000);
-    });
+  $('.menu-toggler').on('click', function () {
+    $(this).toggleClass('open')
+    $('.top-nav').toggleClass('open')
+  })
 
-    $('.up').on('click', function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 2000);
-    });
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open')
+    $('.top-nav').removeClass('open')
+  })
 
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once: false
-    });
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top - 100,
+      },
+      2000
+    )
+  })
 
-});
+  $('.up').on('click', function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      2000
+    )
+  })
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: false,
+  })
+})
